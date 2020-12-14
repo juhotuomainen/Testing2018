3851e7c68f9928d759f4c06469651b17d5b9c618
Juho <juho.tuomainen1@gmail.com>
2020-12-14 16:14:39 +0200

    Lisättiin yksikkötestiluokka ja siihen kaksi testiä Armour (eli Suoja) -luokalle.

---
+++ ArmourTester/ArmourTester.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Testing2018;
+namespace ArmourTester
+{
+    [TestClass]
+    public class ArmourTester
+    {
+        [TestMethod]
+        public void TestLevel1()
+        {
+Armour ar = new Armour("Jorma", "Kokkeli", 20, 2, 2);
+//credating variable lvl for level whose value is a call of the method getLevel() of the armour class.
+int lvl = ar.getLevel();
+if(lvl != 2)
+{ 
+// calling the method Fail of assert class.
+Assert.Fail();        
+}
+}
+        [TestMethod]    
+public void TestSlot1()
+{ 
+Armour ar =new Armour("Jorma", "Kokkeli", 20, 2, 2);
+int slot = ar.getSlot();
+// if clause
+if (slot != 2) 
+{
+// The code fails
+Assert.Fail();
+
+        }
+}
+}
+
+}
+  
---
+++ ArmourTester/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ArmourTester")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ArmourTester")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ba27115b-0302-4232-b682-d623c6dc5797")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
